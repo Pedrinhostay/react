f47b562285575bb8ef74a8eee69917b360b1c2af
Pedro Miguel <pedrostaay@hotmail.com>
2023-07-10 15:44:12 -0300

    todoReact

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,50 +1,61 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+
+import Todo from "./components/Todo.jsx";
+import TodoForm from "./components/TodoForm.jsx";
+import "./App.css";
 
 function App() {
-  const[todos,setTodos] = useState([
+  const [todos, setTodos] = useState([
     {
-      id:1,
+      id: 1,
       text: "Criar funcionalidade X no sistema",
       category: "Trabalho",
       isCompleted: false,
     },
     {
-      id:2,
+      id: 2,
       text: "Ir para a academia",
       category: "Pessoal",
       isCompleted: false,
     },
     {
-      id:3,
+      id: 3,
       text: "Estudar React",
       category: "Estudos",
       isCompleted: false,
     },
-  ])
+  ]);
+  const addTodo = (text, category) => {
+    const newTodos = [
+      ...todos,
+      {
+        id: Math.floor(Math.random() * 10000),
+        text,
+        category,
+        isCompleted: false,
+      },
+    ];
+    setTodos(newTodos);
+  };
 
+  const removeTodo = (id) => {
+    const newTodos = [...todos];
+    const filteredTodos = newTodos.filter((todo) =>
+      todo.id !== id ? todo : null
+    );
+    setTodos(filteredTodos);
+  };
   return (
-    <div className='app'>
+    <div className="app">
       <h1>Lista de tarefas</h1>
       <div className="todo-list">
-        {todos.map((todo) =>(
-          <div className="todo">
-            <div className="content">
-              <p>{todo.text}</p>
-              <p className="category">
-                ({todo.category})
-              </p>
-            </div>
-            <div>
-              <button>Completar</button>
-              <button>X</button>
-            </div>
-          </div>
+        {todos.map((todo) => (
+          <Todo key={todo.id} Todo todo={todo} removeTodo={removeTodo} />
         ))}
       </div>
+      <TodoForm addTodo={addTodo} />
     </div>
-  )
-    
+  );
 }
 
-export default App
+export default App;
